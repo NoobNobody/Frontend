6ddbf9698e01181262071c6fc80b981f0bf3c03b
NoobNobody <stasiu.karwala@gmail.com>
2024-03-26 04:55:43 +0100

    pics adjusted

--- src/utils/categoryImages.jsx
+++ src/utils/categoryImages.jsx
@@ -11,11 +11,28 @@ import dostawca_kurier_miejski from '../assets/dostawca_kurier_miejski.jpg'
 import kontrola_jakosci from '../assets/kontrola_jakosci.jpg'
 import hr from '../assets/hr.jpg'
 import it from '../assets/it.jpg'
-
-import internet_e_commerce from '../assets/internet_e_commerce.jpg'
-import nauka_edukacja_szkolenia from '../assets/nauka_edukacja_szkolenia.jpg'
+import nieruchomosci from '../assets/nieruchomosci.jpg'
+import kadra_kierownicza from '../assets/kadra_kierownicza.jpg'
+import fotografia from '../assets/fotografia.jpg'
+import rolnictwo from '../assets/rolnictwo.jpg'
+import sektor_publiczny from '../assets/sektor_publiczny.jpg'
+import internet from '../assets/internet.jpg'
+import edukacja from '../assets/edukacja.jpg'
 import energetyka from '../assets/energetyka.jpg'
 import finanse_ekonomia_ksiegowosc from '../assets/finanse_ekonomia_ksiegowosc.jpg'
+import biznes from '../assets/biznes.jpg'
+import fryzjer from '../assets/fryzjer.jpg'
+import magazyn from '../assets/magazyn.jpg'
+import produkcja from '../assets/produkcja.jpg'
+import sprzedaz from '../assets/sprzedaz.jpg'
+import kierowca from '../assets/kierowca.jpg'
+import wytworstwo from '../assets/wytworstwo.jpg'
+import gov from '../assets/gov.jpg'
+import srodowisko from '../assets/srodowisko.jpg'
+import prawo from '../assets/prawo.jpg'
+import hostessa from '../assets/hostessa.jpg'
+import gastronomia from '../assets/gastronomia.jpg'
+import laboratorium from '../assets/laboratorium.jpg'
 
 const categoryImages = {
     "Administracja biurowa": administracja_biurowa,
@@ -31,10 +48,28 @@ const categoryImages = {
     "Kontrola jakości": kontrola_jakosci,
     "Human Resources / Zasoby ludzkie": hr,
     "IT / telekomunikacja / Rozwój oprogramowania / Administracja": it,
-    // "Internet / e-Commerce": internet_e_commerce,
-    // "Nauka / Edukacja / Szkolenia": nauka_edukacja_szkolenia,
-    // "Energetyka": energetyka,
-    // "Finanse / Ekonomia / Księgowość": finanse_ekonomia_ksiegowosc
+    "Nieruchomości": nieruchomosci,
+    "Kadra kierownicza": kadra_kierownicza,
+    "Reklama / Grafika / Kreacja / Fotografia": fotografia,
+    "Rolnictwo i ogrodnictwo": rolnictwo,
+    "Sektor publiczny": sektor_publiczny,
+    "Internet / e-Commerce": internet,
+    "Nauka / Edukacja / Szkolenia": edukacja,
+    "Energetyka": energetyka,
+    "Finanse / Ekonomia / Księgowość": finanse_ekonomia_ksiegowosc,
+    "Franczyza / Własny biznes": biznes,
+    "Fryzjerstwo, kosmetyka": fryzjer,
+    "Prace magazynowe": magazyn,
+    "Produkcja": produkcja,
+    "Sprzedaż": sprzedaz,
+    "Transport / Spedycja / Logistyka / Kierowca": kierowca,
+    "Wytwórstwo / Rzemiosło": wytworstwo,
+    "GOV": gov,
+    "BHP / Ochrona środowiska": srodowisko,
+    "Prawo": prawo,
+    "Hostessa, roznoszenie ulotek": hostessa,
+    "Hotelarstwo / Gastronomia / Turystyka": gastronomia,
+    "Laboratorium / Farmacja / Biotechnologia": laboratorium,
 };
 
 export default categoryImages;
